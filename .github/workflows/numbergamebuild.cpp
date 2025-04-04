791220ff3c580fd63a6d63adbb65b383e3bb8d06
riaewo <lera089bikin@gmail.com>
2025-04-04 21:41:05 +1000

    .

---
+++ .github/workflows/numbergamebuild.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+
+    // Инициализация генератора случайных чисел
+    srand(time(0));
+
+    cout << "Новая функция: введите ваше имя: ";
+    string name;
+    cin >> name;
+    cout << "Привет, " << name << "!" << endl;
+
+    // Загадываем число от 1 до 100
+    int secretNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    cout << "Я загадал число от 1 до 100. Попробуй угадать!" << endl;
+
+    do {
+        cout << "Твоя догадка: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess < secretNumber) {
+            cout << "Слишком мало!" << endl;
+        }
+        else if (guess > secretNumber) {
+            cout << "Слишком много!" << endl;
+        }
+        else {
+            cout << "Поздравляю! Ты угадал за " << attempts << " попыток." << endl;
+        }
+    } while (guess != secretNumber);
+
+    return 0;
+}
