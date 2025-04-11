e823596f863120b77462d85d23f9655071d7adce
riaewo <riaewo@users.noreply.github.com>
2025-04-11 13:41:39 +0000

    deploy: 2448f8efce2a392d418626350d52c97decbf3a35

---
+++ search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['numbergamebuild_2ecpp_0',['numbergamebuild.cpp',['../numbergamebuild_8cpp.html',1,'']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "n",
+  1: "n"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files"
+};
+
